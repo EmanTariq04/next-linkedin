6f6c388eb7d9edaef1d82c52d05938f4a4df1540
Eman Tariq <emantariq.1004@gmail.com>
2025-04-29 21:37:15 +0500

    server actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import UserInformation from "@/components/UserInformation";
+import PostForm from "@/components/PostForm";
 
 export default function Home() {
   return (
-    <div className="">
-     <section>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+     <section className="hidden md:inline md:col-span-2">
       <UserInformation />
      </section>
 
-     <section>
-       {/* postform  */}
+     <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+       <PostForm />
        {/* postfeed  */}
      </section>
 
-     <section> 
+     <section className="hidden xl:inline justify-center col-span-2"> 
       {/* widgets  */}
      </section>
     </div>
--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs/server"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 async function UserInformation() {
     const user = await currentUser();
@@ -9,7 +10,7 @@ async function UserInformation() {
     const imageUrl = user?.imageUrl;
 
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center bg-white mr-6 rounded-lg border py-4">
             <Avatar>
                 {user?.id ? (
                     <AvatarImage src={imageUrl} />
@@ -34,6 +35,28 @@ async function UserInformation() {
                     </p>
                 </div>
             </SignedIn>
+
+            <SignedOut>
+                <div className="text-center space-y-2">
+                    <p className="font-semibold">You are not signed in</p>
+
+                    <Button asChild className="bg-[#0B63c4]" text-white>
+                        <SignInButton>Sign In</SignInButton>
+                    </Button>
+                </div>
+            </SignedOut>
+
+            <hr className="w-full border-gray-200 my-5" />
+
+            <div className="flex justify-between w-full px-4 text-sm">
+                <p className="font-semibold text-gray-400">Posts</p>
+                <p className="text-blue-400">0</p>
+            </div>
+
+            <div className="flex justify-between w-full px-4 text-sm">
+                <p className="font-semibold text-gray-400">Comments</p>
+                <p className="text-blue-400">0</p>
+            </div>
         </div>
     )
 }
